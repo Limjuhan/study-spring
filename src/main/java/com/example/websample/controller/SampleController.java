effbf20eed807c5b7b668eb6a996b9220c21eca6
juhan <starlexy@naver.com>
2024-01-22 22:33:20 +0900

    ccc

--- src/main/java/com/example/websample/controller/SampleController.java
+++ src/main/java/com/example/websample/controller/SampleController.java
@@ -1,7 +1,57 @@
 package com.example.websample.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 public class SampleController {
+
+    @GetMapping(value = "/order/{orderId}")
+    public String getOrder(@PathVariable("orderId") String id) {
+        log.info("Get some order" + id);
+
+        return "orderId: "+ id + " , orderAmount:1000";
+    }
+
+    @DeleteMapping(value = "/order/{orderId}")
+    public String deleteOrder(@PathVariable("orderId") String id) {
+        log.info("Get some order" + id);
+
+        return "Delete orderId: "+ id ;
+    }
+
+    @GetMapping(value = "/order")//param을 여러개받을시
+    public String getOrderWithRequestParam(@RequestParam("orderId") String id,
+                                           @RequestParam("orderAmount") String amount) {
+        log.info("Get some order" + id + ", amount: " + amount);
+
+        return "orderId: "+ id + " , orderAmount:1000" + amount;
+    }
+
+    @PostMapping(value = "/order")
+    public String createOrder(@RequestBody CreateOrderRequest createOrderRequest,
+                              @RequestHeader String userAccountId) {
+        log.info("Create order: " + createOrderRequest + ", " +
+                 "userAccountId: " + userAccountId);
+
+        return "orderId: "+ createOrderRequest.getOrderId() + " , " +
+                "orderAmount:1000" + createOrderRequest.getOrderAmount();
+    }
+
+    @PutMapping(value = "/order")
+    public String createOrder() {
+        log.info("Create order");
+
+        return "order created -> orderId:1, orderAmount:1000";
+    }
+
+    @Data // 롬복. getter, setter자동생성해준다.
+    public static class CreateOrderRequest {
+        private String orderId;
+        private Integer orderAmount;
+
+
+    }
 }
