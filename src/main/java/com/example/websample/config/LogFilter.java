a33ae6a5e7a0328322753185298dd2a028d755a0
juhan <starlexy@naver.com>
2024-01-23 19:27:17 +0900

    commit

---
+++ src/main/java/com/example/websample/config/LogFilter.java
@@ -0,0 +1,21 @@
+package com.example.websample.config;
+
+import jakarta.servlet.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+public class LogFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+        log.info("Hello Logfilter: " + Thread.currentThread());
+        // 외부 -> filter -> controller(처리) -> filter -> 외부
+        chain.doFilter(request, response);//chain이 controller처리부분 작업을 위해서는 연결해줘야된다.
+        log.info("Bye Logfilter: " + Thread.currentThread());
+
+    }
+}
---
+++ src/main/java/com/example/websample/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.example.websample.config;
+
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean loggingFilter() {
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new LogFilter());
+        filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.addUrlPatterns("/*");//filter가 사용될 url패턴 지정가능.
+
+        return filterFilterRegistrationBean;
+    }
+
+}
